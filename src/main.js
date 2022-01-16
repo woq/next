0c7307c65ff90ab2ad59bef19bc909da5913b001
greateve <greateve@qq.com>
2022-01-16 18:35:09 +0800

    remove tailwindcss

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-import "./assets/tailwind.css";
 
 // Element UI
 import Element from "element-ui";
